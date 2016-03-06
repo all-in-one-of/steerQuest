5d0521f5b9c22eaa638670158e65db1a00a11c57
Nishantgoyal <nishnt.goyal0789@gmail.com>
2016-03-06 11:00:10 +0530

    Updated socket

--- io/new_client.cpp
+++ io/new_client.cpp
@@ -20,67 +20,52 @@ void error(const char* msg)
 
 int main(int argc, char* argv[])
 {
-
 	int time, sheep_id, x, y;
 	char data[4096];
 	int sockfd, portno, n;
-    struct sockaddr_in serv_addr;
-    struct hostent *server;
-    char buffer[256];
+	struct sockaddr_in serv_addr;
+	struct hostent *server;
+	char buffer[256];
 	char *message;
 
-    if (argc < 3) {
-       fprintf(stderr,"usage %s hostname port\n", argv[0]);
-       exit(0);
-    }
-
-    portno = atoi(argv[2]);
-    server = gethostbyname(argv[1]);
-    if (server == NULL) {
-        fprintf(stderr,"ERROR, no such host\n");
-        exit(0);
-    }
-    
-
-bzero((char *) &serv_addr, sizeof(serv_addr));
-    serv_addr.sin_family = AF_INET;
-    bcopy((char *)server->h_addr, 
-         (char *)&serv_addr.sin_addr.s_addr,
-         server->h_length);
-    serv_addr.sin_port = htons(portno);
-
-FILE* in;
-in = fopen("./sim_data.txt", "r");
-
-while (fgets(data, sizeof(data), in) != NULL){
-//while ((read = getline(&data, &len, in)) != -1){
-printf("Sending: %s\n", data);   
-
-sockfd = socket(AF_INET, SOCK_STREAM, 0);
-if (sockfd < 0) 
-        error("ERROR opening socket");
-
-if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) 
-        error("ERROR connecting");    
-
-	
-	if( send(sockfd , data, strlen(data),0 )<0){
-		cout<<"Send failed";
-		return 1;
+    	if (argc < 3) {
+       		fprintf(stderr,"usage %s hostname port\n", argv[0]);
+       		exit(0);
+    	}
+	portno = atoi(argv[2]);
+    	server = gethostbyname(argv[1]);
+    	if (server == NULL) {
+        	fprintf(stderr,"ERROR, no such host\n");
+        	exit(0);
+    	}
+    	bzero((char *) &serv_addr, sizeof(serv_addr));
+    	serv_addr.sin_family = AF_INET;
+    	bcopy((char *)server->h_addr, (char *)&serv_addr.sin_addr.s_addr,server->h_length);
+    	serv_addr.sin_port = htons(portno);
+	FILE* in;
+	in = fopen("./sim_data.txt", "r");
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if (sockfd < 0) 
+	        error("ERROR opening socket");
+	if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) 
+	        error("ERROR connecting");    
+	while (fgets(data, sizeof(data), in) != NULL){
+		//while ((read = getline(&data, &len, in)) != -1){
+		printf("Sending: %s\n", data);   
+		if( send(sockfd , data, strlen(data),0 )<0){
+			cout<<"Send failed";
+			return 1;
+		}
 	}
-}
-
-fclose(in);
-
-/*ifstream infile("./sim_data.txt"); 
-while (infile >> time >> sheep_id >> x >> y){
+	fclose(in);
+	/*ifstream infile("./sim_data.txt"); 
+	while (infile >> time >> sheep_id >> x >> y){
 	if( send(sockfd , itoa(sheep_id,data,10), strlen(data) , 0) < 0){
 		cout<<"Send failed";
 		return 1;
 	}
-}*/
-
-return 0;
+	}*/
+	return 0;
 }
 
 
--- io/server_2.7.py
+++ io/server_2.7.py
@@ -13,17 +13,16 @@ serversocket.bind(('localhost', port))
 #become a server socket
 serversocket.listen(5)
 
+(clientsocket, address) = serversocket.accept()
+print 'Client Connected'
+
 while 1:
 	#accept connections from outside
-	(clientsocket, address) = serversocket.accept()
-	print 'Client Connected'
 	#serversocket.setblocking(False)
 	#now do something with the clientsocket
-    #inthis case, we'll pretend this is a threaded server
-    	
-	
+    #inthis case, we'll pretend this is a threaded server	
 	data = clientsocket.recv(2048)
 	if not data: break
 	print data
-	clientsocket.close()	
+clientsocket.close()	
 
